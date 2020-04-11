5bdce7b9bd000e59c043aa2886f337d2674f36f8
ajlopez <webmaster@ajlopez.com>
2020-04-11 08:12:46 -0300

    Fixing number of lines in xfunction and yfunction

--- test/xfunction.js
+++ test/xfunction.js
@@ -5,11 +5,16 @@ exports['create xfunction as object'] = function (test) {
 	const fn = function (x) { return x * 2; };
     const from = 0;
     const to = 10;
-	const step = 0.01;
+	const step = 1;
 
     const xfunction = drawie.xfunction(fn, from, to, step, { color: "red" });
 	
     test.ok(xfunction);
     test.equal(typeof xfunction, 'object');
+
+    test.ok(xfunction.elements());
+	test.ok(Array.isArray(xfunction.elements()));
+	test.equal(xfunction.elements().length, 10);
+	test.deepEqual(xfunction.style(), { color: "red" });
 };
 
--- test/yfunction.js
+++ test/yfunction.js
@@ -5,11 +5,16 @@ exports['create yfunction as object'] = function (test) {
 	const fn = function (y) { return y * 2; };
     const from = 0;
     const to = 10;
-	const step = 0.01;
+	const step = 1;
 
     const yfunction = drawie.yfunction(fn, from, to, step, { color: "red" });
 	
     test.ok(yfunction);
     test.equal(typeof yfunction, 'object');
+    
+    test.ok(yfunction.elements());
+	test.ok(Array.isArray(yfunction.elements()));
+	test.equal(yfunction.elements().length, 10);
+	test.deepEqual(yfunction.style(), { color: "red" });
 };
 
