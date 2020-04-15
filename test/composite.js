fe55426a0922dbec692c5a54309e55c20e21de13
ajlopez <webmaster@ajlopez.com>
2020-04-15 06:22:31 -0300

    Additional composite checks in test

--- test/composite.js
+++ test/composite.js
@@ -2,11 +2,12 @@
 const drawie = require('..');
 
 exports['create composite as object'] = function (test) {
+    const style = { color: "red" };
     const from = drawie.point(1, 42);
     const to = drawie.point(2, 3);
-    const line = drawie.line(from, to, { color: "red" });
+    const line = drawie.line(from, to, style);
 	
-	const composite = drawie.composite([line], { color: "red" });
+	const composite = drawie.composite([line], style);
     
     test.ok(composite);
     test.equal(typeof composite, 'object');
@@ -14,5 +15,13 @@ exports['create composite as object'] = function (test) {
     test.ok(composite.elements());
 	test.ok(Array.isArray(composite.elements()));
 	test.equal(composite.elements().length, 1);
-	test.deepEqual(composite.style(), { color: "red" });
+	test.deepEqual(composite.style(), style);
+	test.deepEqual(line.style(), style);
+    
+    composite.style().color = "blue";
+    line.style().color = "yellow";
+    
+    test.deepEqual(style, { color : "red" });
+	test.deepEqual(composite.style(), { color : "blue" });
+	test.deepEqual(line.style(), { color : "yellow" });
 };
