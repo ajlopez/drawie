caeddc8c3d9cf8261264a2047d7f1b389c0ddcfe
ajlopez <webmaster@ajlopez.com>
2020-04-16 07:36:07 -0300

    Additional check in rectangle test

--- test/rectangle.js
+++ test/rectangle.js
@@ -2,11 +2,19 @@
 const drawie = require('..');
 
 exports['create rectangle as object'] = function (test) {
+    const style = { color: "red" };
     const from = drawie.point(1, 42);
     const to = drawie.point(2, 3);
 
-    const rectangle = drawie.rectangle(from, to, { color: "red" });
+    const rectangle = drawie.rectangle(from, to, style);
 	
     test.ok(rectangle);
     test.equal(typeof rectangle, 'object');
+    
+    test.deepEqual(rectangle.style(), style);
+    
+    rectangle.style().color = "blue";
+    
+    test.deepEqual(rectangle.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
 };
