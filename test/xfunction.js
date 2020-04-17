264a8c1ed832eed05440aa2c5e1574025a7740d0
ajlopez <webmaster@ajlopez.com>
2020-04-17 13:00:52 -0300

    Additional check in xfunction test

--- test/xfunction.js
+++ test/xfunction.js
@@ -6,8 +6,9 @@ exports['create xfunction as object'] = function (test) {
     const from = 0;
     const to = 10;
 	const step = 1;
+    const style = { color: "red" };
 
-    const xfunction = drawie.xfunction(fn, from, to, step, { color: "red" });
+    const xfunction = drawie.xfunction(fn, from, to, step, style);
 	
     test.ok(xfunction);
     test.equal(typeof xfunction, 'object');
@@ -15,6 +16,11 @@ exports['create xfunction as object'] = function (test) {
     test.ok(xfunction.elements());
 	test.ok(Array.isArray(xfunction.elements()));
 	test.equal(xfunction.elements().length, 10);
-	test.deepEqual(xfunction.style(), { color: "red" });
+	test.deepEqual(xfunction.style(), style);
+    
+    xfunction.style().color = "blue";
+    
+    test.deepEqual(xfunction.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
 };
 
