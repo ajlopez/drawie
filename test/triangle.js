e7926a4611a794bd2d34e73952b19fc4210f2f91
ajlopez <webmaster@ajlopez.com>
2020-04-19 09:54:26 -0300

    Additional check in triangle test

--- test/triangle.js
+++ test/triangle.js
@@ -2,12 +2,20 @@
 const drawie = require('..');
 
 exports['create triangle as object'] = function (test) {
+    const style = { color: "red" };
     const from = drawie.point(1, 42);
     const to = drawie.point(2, 3);
     const to2 = drawie.point(3, 3);
 
-    const triangle = drawie.triangle(from, to, to2, { color: "red" });
+    const triangle = drawie.triangle(from, to, to2, style);
 	
     test.ok(triangle);
     test.equal(typeof triangle, 'object');
+    
+    test.deepEqual(triangle.style(), style);
+    
+    triangle.style().color = "blue";
+    
+    test.deepEqual(triangle.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
 };
