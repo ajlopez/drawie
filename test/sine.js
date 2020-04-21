94cfe2fafdff779fe578b1d61ebded837c55c059
ajlopez <webmaster@ajlopez.com>
2020-04-21 07:12:56 -0300

    Add additional check to sine test

--- test/sine.js
+++ test/sine.js
@@ -2,13 +2,21 @@
 const drawie = require('..');
 
 exports['create sine as object'] = function (test) {
+    const style = { color: "red" };
     const from = 0;
     const to = 10;
 	const step = 0.01;
 
-    const sine = drawie.sine(from, to, step, { color: "red" });
+    const sine = drawie.sine(from, to, step, style);
 	
     test.ok(sine);
     test.equal(typeof sine, 'object');
+    
+    test.deepEqual(sine.style(), style);
+    
+    sine.style().color = "blue";
+    
+    test.deepEqual(sine.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
 };
 
