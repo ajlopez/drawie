2c6e726d872aa0c77ac9805fec875632f105b1e9
ajlopez <webmaster@ajlopez.com>
2020-04-23 05:53:45 -0300

    Additional check in pfunction test

--- test/pfunction.js
+++ test/pfunction.js
@@ -5,11 +5,22 @@ exports['create pfunction as object'] = function (test) {
 	const fn = function (n) { return drawie.point(n, n * 2); };
     const from = 0;
     const to = 10;
-	const step = 0.01;
+	const step = 1;
+    const style = { color: "red" };
 
-    const pfunction = drawie.pfunction(fn, from, to, step, { color: "red" });
+    const pfunction = drawie.pfunction(fn, from, to, step, style);
 	
     test.ok(pfunction);
     test.equal(typeof pfunction, 'object');
+    
+    test.ok(pfunction.elements());
+	test.ok(Array.isArray(pfunction.elements()));
+	test.equal(pfunction.elements().length, 10);
+	test.deepEqual(pfunction.style(), style);
+    
+    pfunction.style().color = "blue";
+    
+    test.deepEqual(pfunction.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
 };
 
