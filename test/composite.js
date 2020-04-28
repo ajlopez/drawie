1c07dc9fcb5ce7dab95a1a67e46968da0e9b705c
ajlopez <webmaster@ajlopez.com>
2020-04-28 09:00:00 -0300

    Composite mirror

--- test/composite.js
+++ test/composite.js
@@ -25,3 +25,26 @@ exports['create composite as object'] = function (test) {
 	test.deepEqual(composite.style(), { color : "blue" });
 	test.deepEqual(line.style(), { color : "yellow" });
 };
+
+exports['mirror composite'] = function (test) {
+    const style = { color: "red" };
+    const from = drawie.point(1, 42);
+    const to = drawie.point(2, 3);
+    const line = drawie.line(from, to, style);
+	
+	const composite = drawie.composite([line], style).mirror();
+    
+    test.ok(composite);
+    test.equal(typeof composite, 'object');
+
+    test.ok(composite.elements());
+	test.ok(Array.isArray(composite.elements()));
+	test.equal(composite.elements().length, 1);
+	test.deepEqual(composite.style(), style);
+	test.deepEqual(line.style(), style);
+    
+    test.equal(composite.elements()[0].from().x(), -1);
+    test.equal(composite.elements()[0].from().y(), -42);
+    test.equal(composite.elements()[0].to().x(), -2);
+    test.equal(composite.elements()[0].to().y(), -3);
+};
