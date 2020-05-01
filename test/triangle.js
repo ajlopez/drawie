212b0fed0d0c452c68685aa0219d84f5193b4cb2
ajlopez <webmaster@ajlopez.com>
2020-05-01 08:55:56 -0300

    Mirror triangle

--- test/triangle.js
+++ test/triangle.js
@@ -37,3 +37,36 @@ exports['create triangle as object'] = function (test) {
     test.equal(triangle.elements()[2].to().x(), 1);
     test.equal(triangle.elements()[2].to().y(), 42);
 };
+
+exports['mirror triangle'] = function (test) {
+    const style = { color: "red" };
+    const from = drawie.point(1, 42);
+    const to = drawie.point(2, 3);
+    const to2 = drawie.point(3, 3);
+
+    const triangle = drawie.triangle(from, to, to2, style).mirror();
+	
+    test.ok(triangle);
+    test.equal(typeof triangle, 'object');
+    
+    test.deepEqual(triangle.style(), style);
+    
+    test.ok(triangle.elements());
+    test.equal(triangle.elements().length, 3);
+    
+    test.equal(triangle.elements()[0].from().x(), -1);
+    test.equal(triangle.elements()[0].from().y(), -42);
+    test.equal(triangle.elements()[0].to().x(), -2);
+    test.equal(triangle.elements()[0].to().y(), -3);
+    
+    test.equal(triangle.elements()[1].from().x(), -2);
+    test.equal(triangle.elements()[1].from().y(), -3);
+    test.equal(triangle.elements()[1].to().x(), -3);
+    test.equal(triangle.elements()[1].to().y(), -3);
+    
+    test.equal(triangle.elements()[2].from().x(), -3);
+    test.equal(triangle.elements()[2].from().y(), -3);
+    test.equal(triangle.elements()[2].to().x(), -1);
+    test.equal(triangle.elements()[2].to().y(), -42);
+};
+
