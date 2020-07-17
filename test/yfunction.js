f7a1238de8e62896ab0d7b251fd9134338db0589
ajlopez <webmaster@ajlopez.com>
2020-07-17 11:04:54 -0300

    Mirror yfunction

--- test/yfunction.js
+++ test/yfunction.js
@@ -24,3 +24,34 @@ exports['create yfunction as object'] = function (test) {
     test.deepEqual(style, { color: "red" });
 };
 
+exports['mirror yfunction'] = function (test) {
+	const fn = function (y) { return y * 2; };
+    const from = 0;
+    const to = 10;
+	const step = 1;
+    const style = { color: "red" };
+
+    const yfunction0 = drawie.yfunction(fn, from, to, step, style);
+    const yfunction = yfunction0.mirror();
+	
+    test.ok(yfunction);
+    test.equal(typeof yfunction, 'object');
+
+    test.ok(yfunction.elements());
+	test.ok(Array.isArray(yfunction.elements()));
+	test.equal(yfunction.elements().length, 10);
+	test.deepEqual(yfunction.style(), style);
+    
+    yfunction.style().color = "blue";
+    
+    test.deepEqual(yfunction.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
+    
+    for (let k = 0; k < yfunction.elements().length; k++) {
+        test.equal(-yfunction0.elements()[k].from().x(), yfunction.elements()[k].from().x());
+        test.equal(-yfunction0.elements()[k].from().y(), yfunction.elements()[k].from().y());
+        test.equal(-yfunction0.elements()[k].to().x(), yfunction.elements()[k].to().x());
+        test.equal(-yfunction0.elements()[k].to().y(), yfunction.elements()[k].to().y());
+    }
+};
+
