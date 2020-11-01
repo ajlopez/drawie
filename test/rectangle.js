d4ef378adc63c244a0e26efccfaacca3734c9316
ajlopez <webmaster@ajlopez.com>
2020-11-01 12:59:30 -0300

    Create rectangle using width and height

--- test/rectangle.js
+++ test/rectangle.js
@@ -42,6 +42,42 @@ exports['create rectangle as object'] = function (test) {
     test.equal(rectangle.elements()[3].to().y(), 42);
 };
 
+exports['create rectangle using width and height'] = function (test) {
+    const style = { color: "red" };
+    const width = 100;
+    const height = 50;
+
+    const rectangle = drawie.rectangle(width, height, style);
+	
+    test.ok(rectangle);
+    test.equal(typeof rectangle, 'object');
+    
+    test.deepEqual(rectangle.style(), style);
+    
+    test.ok(rectangle.elements());
+    test.equal(rectangle.elements().length, 4);
+
+    test.equal(rectangle.elements()[0].from().x(), -50);
+    test.equal(rectangle.elements()[0].from().y(), 25);
+    test.equal(rectangle.elements()[0].to().x(), 50);
+    test.equal(rectangle.elements()[0].to().y(), 25);
+
+    test.equal(rectangle.elements()[1].from().x(), 50);
+    test.equal(rectangle.elements()[1].from().y(), 25);
+    test.equal(rectangle.elements()[1].to().x(), 50);
+    test.equal(rectangle.elements()[1].to().y(), -25);
+
+    test.equal(rectangle.elements()[2].from().x(), 50);
+    test.equal(rectangle.elements()[2].from().y(), -25);
+    test.equal(rectangle.elements()[2].to().x(), -50);
+    test.equal(rectangle.elements()[2].to().y(), -25);
+
+    test.equal(rectangle.elements()[3].from().x(), -50);
+    test.equal(rectangle.elements()[3].from().y(), -25);
+    test.equal(rectangle.elements()[3].to().x(), -50);
+    test.equal(rectangle.elements()[3].to().y(), 25);
+};
+
 exports['mirror rectangle'] = function (test) {
     const style = { color: "red" };
     const from = drawie.point(1, 42);
