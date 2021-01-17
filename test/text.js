3e89d17e41b97b771f022b921b61202953f27c5c
ajlopez <webmaster@ajlopez.com>
2021-01-17 14:50:25 -0300

    Text copy style

---
+++ test/text.js
@@ -0,0 +1,22 @@
+
+const drawie = require('..');
+
+exports['create text as object'] = function (test) {
+    const style = { color: "red" };
+    const text = drawie.text(10, 20, "Hello", style);;
+    
+    test.ok(text);
+    test.equal(typeof text, 'object');
+	
+	test.equal(text.x(), 10);
+	test.equal(text.y(), 20);
+	test.equal(text.text(), 'Hello');
+	
+    test.deepEqual(text.style(), style);
+    
+    text.style().color = "blue";
+    
+    test.deepEqual(text.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
+};
+
