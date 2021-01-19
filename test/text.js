4909ce03f36bb987f72b5fc84232db45e97e4780
ajlopez <webmaster@ajlopez.com>
2021-01-19 17:05:45 -0300

    Text clone

--- test/text.js
+++ test/text.js
@@ -20,3 +20,24 @@ exports['create text as object'] = function (test) {
     test.deepEqual(style, { color: "red" });
 };
 
+exports['clone text'] = function (test) {
+    const style = { color: "red" };
+    const original = drawie.text(10, 20, "Hello", style);;
+    const text = original.clone();
+    
+    test.ok(text);
+    test.equal(typeof text, 'object');
+    test.ok(text !== original);
+	
+	test.equal(text.x(), 10);
+	test.equal(text.y(), 20);
+	test.equal(text.text(), 'Hello');
+	
+    test.deepEqual(text.style(), style);
+    
+    text.style().color = "blue";
+    
+    test.deepEqual(text.style(), { color: "blue" });
+    test.deepEqual(style, { color: "red" });
+};
+
