7f29e3a32c82fa3a11a1365b6b044dd2bd9a9f4c
ajlopez <webmaster@ajlopez.com>
2021-02-20 19:33:57 -0300

    Translate 3d polygon

--- test/polygon3d.js
+++ test/polygon3d.js
@@ -32,3 +32,36 @@ exports['create polygon as object'] = function (test) {
     test.equal(polygon.elements()[3].z(), 0);
 };
 
+exports['translate polygon'] = function (test) {
+    const p1 = drawie.point(0, 0, 0);
+    const p2 = drawie.point(1, 0, 0);
+    const p3 = drawie.point(1, 1, 0);
+    const p4 = drawie.point(0, 1, 0);
+    
+    const pm = drawie.point(1, 2, 3);
+
+    const polygon = drawie.polygon([ p1, p2, p3, p4 ]).translate(pm);
+	
+    test.ok(polygon);
+    test.equal(typeof polygon, 'object');
+    
+    test.ok(polygon.elements());
+    test.equal(polygon.elements().length, 4);
+    
+    test.equal(polygon.elements()[0].x(), 1);
+    test.equal(polygon.elements()[0].y(), 2);
+    test.equal(polygon.elements()[0].z(), 3);
+    
+    test.equal(polygon.elements()[1].x(), 2);
+    test.equal(polygon.elements()[1].y(), 2);
+    test.equal(polygon.elements()[1].z(), 3);
+    
+    test.equal(polygon.elements()[2].x(), 2);
+    test.equal(polygon.elements()[2].y(), 3);
+    test.equal(polygon.elements()[2].z(), 3);
+    
+    test.equal(polygon.elements()[3].x(), 1);
+    test.equal(polygon.elements()[3].y(), 3);
+    test.equal(polygon.elements()[3].z(), 3);
+};
+
