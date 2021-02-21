1172f680c9c4650ed405b694414c402c75641180
ajlopez <webmaster@ajlopez.com>
2021-02-21 16:26:12 -0300

    Resize 3d triangle

--- test/triangle3d.js
+++ test/triangle3d.js
@@ -40,3 +40,26 @@ exports['get normal'] = function (test) {
     test.equal(normal.z(), 1);
 };
 
+exports['resize triangle'] = function (test) {
+    const from = drawie.point(1, 42, 0);
+    const to = drawie.point(2, 3, 0);
+    const to2 = drawie.point(3, 3, 0);
+
+    const triangle = drawie.triangle(from, to, to2).resize(2);
+	
+    test.ok(triangle);
+    test.equal(typeof triangle, 'object');
+    
+    test.equal(triangle.v1().x(), 2);
+    test.equal(triangle.v1().y(), 84);
+    test.equal(triangle.v1().z(), 0);
+    
+    test.equal(triangle.v2().x(), 4);
+    test.equal(triangle.v2().y(), 6);
+    test.equal(triangle.v2().z(), 0);
+    
+    test.equal(triangle.v3().x(), 6);
+    test.equal(triangle.v3().y(), 6);
+    test.equal(triangle.v3().z(), 0);
+};
+
