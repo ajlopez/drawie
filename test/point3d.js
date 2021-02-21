14c4daf3074c0feb1b1a25bb2fd98c50c3955057
ajlopez <webmaster@ajlopez.com>
2021-02-21 17:46:05 -0300

    Point 3d rotate x axis

--- test/point3d.js
+++ test/point3d.js
@@ -199,3 +199,69 @@ exports['rotate point 45 degrees y axis'] = function (test) {
     test.equal(result.z(), z);
 };
 
+exports['rotate point 0 degrees x axis'] = function (test) {
+    const point = drawie.point(3, 1, 42);
+
+	const result = point.xrotate(0);
+	
+    test.equal(result.x(), 3);
+    test.equal(result.y(), 1);
+    test.equal(result.z(), 42);
+};
+
+exports['rotate point 90 degrees x axis'] = function (test) {
+    const point = drawie.point(3, 1, 42);
+
+	const result = point.xrotate(90);
+	
+    test.equal(result.x(), 3);
+    test.equal(result.y(), -42);
+    test.equal(result.z(), 1);
+};
+
+exports['rotate point 180 degrees x axis'] = function (test) {
+    const point = drawie.point(3, 1, 42);
+
+	const result = point.xrotate(180);
+	
+    test.equal(result.x(), 3);
+    test.equal(result.y(), -1);
+    test.equal(result.z(), -42);
+};
+
+exports['rotate point 270 degrees x axis'] = function (test) {
+    const point = drawie.point(3, 1, 42);
+
+	const result = point.xrotate(270);
+	
+    test.equal(result.x(), 3);
+    test.equal(result.y(), 42);
+    test.equal(result.z(), -1);
+};
+
+exports['rotate point -90 degrees x axis'] = function (test) {
+    const point = drawie.point(3, 1, 42);
+
+	const result = point.xrotate(-90);
+	
+    test.equal(result.x(), 3);
+    test.equal(result.y(), 42);
+    test.equal(result.z(), -1);
+};
+
+exports['rotate point 45 degrees x axis'] = function (test) {
+    const point = drawie.point(3, 1, 2);
+
+	const result = point.xrotate(45);
+	
+	const cos = Math.cos(2 * Math.PI / 8);
+	const sin = Math.sin(2 * Math.PI / 8);
+	
+	const y = point.y() * cos - point.z() * sin;
+	const z = point.y() * sin + point.z() * cos;
+	
+    test.equal(result.x(), 3);
+    test.equal(result.y(), y);
+    test.equal(result.z(), z);
+};
+
