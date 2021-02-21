f7d1ec4b8dcc968180e93638ff6c7089e7d0b186
ajlopez <webmaster@ajlopez.com>
2021-02-21 17:53:28 -0300

    10x10 squares in STL sample

--- samples/stl/squares.js
+++ samples/stl/squares.js
@@ -11,8 +11,8 @@ const square = drawie.polygon([ p1, p2, p3, p4 ]);
 
 const squares = [];
 
-for (let k = 0; k < 100; k++)
-    for (let j = 0; j < 100; j++)
+for (let k = 0; k < 10; k++)
+    for (let j = 0; j < 10; j++)
         squares.push(square.translate(drawie.point(k * 2, j * 2, 0)));
 
 console.log(convert(squares, 'squares'));
