ead5fd0d06eeb08470faa17ce9e84e6e794e9703
ajlopez <webmaster@ajlopez.com>
2021-02-21 16:29:01 -0300

    Y resize 3d triangle

--- test/triangle3d.js
+++ test/triangle3d.js
@@ -86,3 +86,26 @@ exports['x resize triangle'] = function (test) {
     test.equal(triangle.v3().z(), 0);
 };
 
+exports['y resize triangle'] = function (test) {
+    const from = drawie.point(1, 42, 0);
+    const to = drawie.point(2, 3, 0);
+    const to2 = drawie.point(3, 3, 0);
+
+    const triangle = drawie.triangle(from, to, to2).yresize(2);
+	
+    test.ok(triangle);
+    test.equal(typeof triangle, 'object');
+    
+    test.equal(triangle.v1().x(), 1);
+    test.equal(triangle.v1().y(), 84);
+    test.equal(triangle.v1().z(), 0);
+    
+    test.equal(triangle.v2().x(), 2);
+    test.equal(triangle.v2().y(), 6);
+    test.equal(triangle.v2().z(), 0);
+    
+    test.equal(triangle.v3().x(), 3);
+    test.equal(triangle.v3().y(), 6);
+    test.equal(triangle.v3().z(), 0);
+};
+
