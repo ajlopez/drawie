dd5760176b6a5918f7ec42a6c1a61f5b34219a7f
ajlopez <webmaster@ajlopez.com>
2021-03-11 16:22:50 -0300

    Composite 3d y resize

--- test/composite3d.js
+++ test/composite3d.js
@@ -116,3 +116,32 @@ exports['x resize composite'] = function (test) {
     test.equal(composite.elements()[0].v3().z(), 1);
 };
 
+exports['y resize composite'] = function (test) {
+    const from = drawie.d3.point(1, 42, 0);
+    const to = drawie.d3.point(2, 3, 0);
+    const to2 = drawie.d3.point(3, 3, 1);
+
+    const triangle = drawie.d3.triangle(from, to, to2);
+	
+	const composite = drawie.d3.composite([ triangle ])
+        .yresize(2);
+    
+    test.ok(composite);
+    test.equal(typeof composite, 'object');
+
+    test.ok(composite.elements());
+	test.ok(Array.isArray(composite.elements()));
+	test.equal(composite.elements().length, 1);
+    test.equal(composite.elements()[0].v1().x(), 1);
+    test.equal(composite.elements()[0].v1().y(), 84);
+    test.equal(composite.elements()[0].v1().z(), 0);
+    
+    test.equal(composite.elements()[0].v2().x(), 2);
+    test.equal(composite.elements()[0].v2().y(), 6);
+    test.equal(composite.elements()[0].v2().z(), 0);
+    
+    test.equal(composite.elements()[0].v3().x(), 3);
+    test.equal(composite.elements()[0].v3().y(), 6);
+    test.equal(composite.elements()[0].v3().z(), 1);
+};
+
